46bebdbcb97004cad88ffd4569ea5500b7642daf
ibizaman <ibizapeanut@gmail.com>
2024-06-14 04:46:02 +0200

    make nix haskell lsp work in emacs

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     hakyll-flakes.url = "github:Radvendii/hakyll-flakes";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
   outputs = { nixpkgs, hakyll-flakes, flake-utils, ... }:
@@ -23,6 +24,8 @@
           name = "site";
           src = ./.;
           websiteBuildInputs = with nixpkgs.legacyPackages.${system}; [
+            cabal-install
+            haskell-language-server
             # rubber
             # texlive.combined.scheme-full
             # poppler_utils
